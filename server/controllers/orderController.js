6a2de44f04b9d6f689c7aee73222f86f38fb7022
Anthony Fink <psytonik@icloud.com>
2021-07-03 12:39:27 +0300

    fixing api orders controller getCountOfOrders getSalesStatistic

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -137,7 +137,6 @@ const getSalesStatistic = async (req,res) => {
         }
 
     } catch (err) {
-
         return res.status(500).json({
             error: err.message,
             success: false,
@@ -156,7 +155,6 @@ const getCountOfOrders = async (req,res) => {
         }
         return res.status(200).json({ orderCount });
     } catch (err) {
-        console.error(err.message, 'ERROR => getCountOfOrders');
         return res.status(500).json({
             message: err.message,
             success:false,
