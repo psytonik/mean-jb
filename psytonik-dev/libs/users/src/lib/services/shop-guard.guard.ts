37c84e163f6fbd73fc9872873fa69a66061c89ed
Anthony Fink <psytonik@icloud.com>
2021-07-08 21:29:37 +0300

    adding new guard for shop

--- psytonik-dev/libs/users/src/lib/services/shop-guard.guard.ts
+++ psytonik-dev/libs/users/src/lib/services/shop-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { CanActivate, Router, UrlTree } from "@angular/router";
 import { Observable } from 'rxjs';
 import { AuthService, LocalStorageService } from "@psytonik-dev/users";
 
@@ -15,8 +15,7 @@ export class ShopGuardGuard implements CanActivate {
   ) {
   }
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if(this.authService.isAuthenticated()){
       return true;
     } else {
